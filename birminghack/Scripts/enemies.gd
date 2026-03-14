3c08d630f2aa82bc9126c4a5f3c32071d7f263aa
Bros112 <thebros112@outlook.com>
2026-03-14 17:56:43 +0000

    add experience orb logic

--- birminghack/Scripts/enemies.gd
+++ birminghack/Scripts/enemies.gd
@@ -7,7 +7,7 @@ var parent
 
 signal collision
 
-signal collision
+var xp_scene: PackedScene = load("res://Scenes/ExperienceOrb.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,6 +19,7 @@ func _process(delta: float) -> void:
 	var vector_to_player = player.position - position
 	
 	if vector_to_player.length() < 50: 
+		summonXP()
 		queue_free()
 	
 	vector_to_player = vector_to_player.normalized()
@@ -30,3 +31,8 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	print("Body Entered")
 	collision.emit()
+
+func summonXP() -> void:
+	var xp = xp_scene.instantiate()
+	get_tree().current_scene.add_child(xp)
+	xp.initiate(self)
