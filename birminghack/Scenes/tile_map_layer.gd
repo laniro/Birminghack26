e33df538eeaa19099c65fe152e7a3c4db0cc7973
BinkyBo <wmarshall098@gmail.com>
2026-03-14 18:57:33 +0000

    background

---
+++ birminghack/Scenes/tile_map_layer.gd
@@ -0,0 +1,94 @@
+extends TileMapLayer
+
+var alt = FastNoiseLite.new()
+var temp = FastNoiseLite.new()
+var moisture = FastNoiseLite.new()
+
+const CHUNK_SIZE = 128
+const TILEDIM = 16
+const CHUNKLOADTHRESHHOLD = 3000
+
+const GRASS = Vector2(0,0)
+const FOREST = Vector2(1,0)
+const STONE = Vector2(0,1)
+const PATH = Vector2(1,1)
+
+@onready var player = $"../Player"
+
+var generated_chunks = {}
+var chunk_queue = []
+
+func _ready() -> void:
+	_initialize_noise()
+	generated_chunk(Vector2(0,0))
+
+func _physics_process(delta: float) -> void:
+	check_chunk_loading()
+	process_chunk_queue()
+	update_position_label()
+
+func _initialize_noise():
+	moisture.seed = randi()
+	temp.seed = randi()
+	alt.seed = randi()
+	
+func generated_chunk(chunk_pos:Vector2i):
+	if chunk_pos in generated_chunks:
+		return
+	generated_chunks[chunk_pos] = true
+	chunk_queue.append(chunk_pos)
+	
+func process_chunk_queue():
+	while !chunk_queue.is_empty():
+		var chunk_pos = chunk_queue.pop_front()
+		var chunkoffset = chunk_pos * CHUNK_SIZE
+		
+		for x in range(CHUNK_SIZE):
+			for y in range(CHUNK_SIZE):
+				var wx = chunkoffset.x +x
+				var wy = chunkoffset.y + y
+				
+				var a = alt.get_noise_2d(wx,wy)*30
+				var m = moisture.get_noise_2d(wx,wy)*30
+				
+				var atlas = FOREST
+				if (a<-2 and m<-6) or a>7:
+					atlas = STONE
+				elif m < 0:
+					atlas = PATH
+				elif m < 4:
+					atlas = GRASS
+				
+				
+				set_cell(Vector2i(wx,wy),0, atlas)
+		
+	
+
+func check_chunk_loading():
+	if not is_instance_valid(player):
+		return
+	var player_chunk = Vector2i(floor(player.global_position.x/(CHUNK_SIZE*TILEDIM)),floor(player.global_position.y/(CHUNK_SIZE*TILEDIM)))
+	
+	var lx = posmod(player.global_position.x,CHUNK_SIZE*TILEDIM)
+	var ly = posmod(player.global_position.y,CHUNK_SIZE*TILEDIM)
+	
+	generated_chunk(player_chunk+Vector2i(-1,0))
+	generated_chunk(player_chunk+Vector2i(1,0))
+	generated_chunk(player_chunk+Vector2i(0,-1))
+	generated_chunk(player_chunk+Vector2i(0,1))
+	generated_chunk(player_chunk+Vector2i(-1,1))
+	generated_chunk(player_chunk+Vector2i(1,1))
+	generated_chunk(player_chunk+Vector2i(-1,-1))
+	generated_chunk(player_chunk+Vector2i(1,-1))
+	
+	
+	
+func update_position_label():
+	if is_instance_valid(player):
+		var cx = floor(player.global_position.x/(CHUNK_SIZE*TILEDIM))
+		var cy = floor(player.global_position.y/(CHUNK_SIZE*TILEDIM))
+
+	
+	
+	
+	
