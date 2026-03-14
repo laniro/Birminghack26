2c04d11102d848fba78bc7197fdb923b7de8d367
cal <crowleon758@gmail.com>
2026-03-14 12:34:45 +0000

    Player Stuff

--- birminghack/Scripts/player.gd
+++ birminghack/Scripts/player.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
 @export var speed = 400
+@export var time = 100
 var screensize
+
+signal hit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,4 +22,19 @@ func _process(delta: float) -> void:
 		velocity.x = 1
 	elif Input.is_action_pressed("moveLeft"):
 		velocity.x = -1
-	
+	velocity = velocity.normalized() * speed
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screensize)
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hit.emit()
+
+func onKill():
+	time += 5
+
+func _on_timer_timeout() -> void:
+	time -= 1
+	if time == 0:
+		pass # Where we will add shift code
