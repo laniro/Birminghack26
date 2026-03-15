73fc09793c4af40b1708376d1353d6c1c485b0f8
Bros112 <thebros112@outlook.com>
2026-03-15 06:21:59 +0000

    idk anymore

--- birminghack/Scripts/orb.gd
+++ birminghack/Scripts/orb.gd
@@ -55,5 +55,5 @@ func _process(delta: float) -> void:
 		queue_free()
 
 
-func _on_area_entered(area: Area2D) -> void:
+func _on_area_entered(_area: Area2D) -> void:
 	queue_free()
--- birminghack/Scripts/poison.gd
+++ birminghack/Scripts/poison.gd
@@ -11,7 +11,7 @@ func _on_area_entered(area: Area2D) -> void:
 		$Tick.start()
 
 
-func _on_area_exited(area: Area2D) -> void:
+func _on_area_exited(_area: Area2D) -> void:
 	damage = false
 
 
